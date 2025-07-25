d66ec90a30e38a2104f3f2f5e61f6a194a95b714
Ferdinand <ferdinand.rohlfing@googlemail.com>
2025-07-25 12:32:05 +0200

    Update docs and add argument tests

--- tests/args/test_args.py
+++ tests/args/test_args.py
@@ -28,12 +28,61 @@ class ArgSourceParentSerializer(NestedSerializer):
         fields = ("id", "foo")
 
 
+class RequiredNestedModel(models.Model):
+    pass
+
+
+class RequiredParentModel(models.Model):
+    nested = models.ForeignKey(RequiredNestedModel, on_delete=models.CASCADE)
+
+
+class RequiredNestedSerializer(ModelSerializer):
+    class Meta:
+        model = RequiredNestedModel
+        fields = ("id",)
+
+
+class RequiredParentSerializer(NestedSerializer):
+    nested = RequiredNestedSerializer(required=True)
+
+    class Meta:
+        model = RequiredParentModel
+        fields = ("id", "nested")
+
+
+class NotRequiredParentSerializer(NestedSerializer):
+    nested = RequiredNestedSerializer(required=False)
+
+    class Meta:
+        model = RequiredParentModel
+        fields = ("id", "nested")
+
+
+class AllowNullNestedModel(models.Model):
+    pass
+
+
+class AllowNullParentModel(models.Model):
+    nested = models.ForeignKey(AllowNullNestedModel, on_delete=models.CASCADE)
+
+
+class AllowNullNestedSerializer(ModelSerializer):
+    class Meta:
+        model = AllowNullNestedModel
+        fields = ("id",)
+
+
 class ArgTest(TestCase):
     def test_required(self):
-        pass
+        data = {}
+        expected_valid = False
+        serializer = RequiredParentSerializer(data=data)
+        assert serializer.is_valid() == expected_valid, serializer.errors
 
-    def test_allow_null(self):
-        pass
+        data = {}
+        expected_valid = True
+        serializer = NotRequiredParentSerializer(data=data)
+        assert serializer.is_valid() == expected_valid, serializer.errors
 
     def test_allow_empty(self):
         pass
@@ -54,6 +103,3 @@ class ArgTest(TestCase):
         instance = serializer.save()
         result = ArgSourceParentSerializer(instance=instance).data
         assert result == expected, f"\nResult:   {result}\nExpected: {expected}"
-
-    def test_delete_on_remove(self):
-        pass
